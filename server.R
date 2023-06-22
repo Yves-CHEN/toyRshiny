8355003d9dde857b5f4013a4778d80b2071542aa
Wenhan <chenwh795@gmail.com>
2023-06-23 09:35:08 +1000

    fix.bug

--- server.R
+++ server.R
@@ -1,95 +1,47 @@
-library(data.table)
 library(randomForest)
+library(shiny)
 
+
+options(shiny.maxRequestSize = 9* 1024^2)
 # Read in the RF model
-model <- readRDS("model.rds")
+
 
 server <- function(input, output) {
-
-  # Define the function that will process the file
-  generate <- function(featureFile) {
-    #load RF model
-    load("RF.model.Mar2023.dat")
-    model = models[["full"]]
-    #dat = read_table2(featureFile)
-    dat = read.table(featureFile,header =T, as.is =T)
-    pred_persistent <- predict(model, dat,  type='prob')
-    pred_persistent
-
-    #return(paste("File processed: ", featureFile))
-  }
-
-
-   observeEvent(input$process, {
-    if (is.null(input$file)) return(NULL)
-    shinyjs::disable("process")
-    # notify the user that the file is being processed
-    showModal(modalDialog(
-      title = "Please wait",
-      "Your file is being processed ...",
-      easyClose = FALSE
-    ))
-
-    # Read the file
-    inFile <- input$file
-    result <- generate(inFile$datapath)
-    unlink(inFile$datapath)
-    # Update the result output
-    output$result <- renderPrint({
-      result
+    is_processing <- reactiveVal(FALSE)
+    output$is_processing <- reactive({
+        is_processing()
     })
+    outputOptions(output, "is_processing", suspendWhenHidden = FALSE)
+    # Define the function that will process the file
+    generate <- function(featureFile) {
+        #load RF model
+        load("../17.predictPersistent//model_foldEnrich/RF.model.Mar2023.dat")
+        model = models[["full"]]
+        #dat = read_table2(featureFile)
+        dat = read.table(featureFile,header =T, as.is =T)
+        pred_persistent <- predict(model, dat,  type='prob')
+        pred_persistent [, 2]
+    }
 
-    # Re-enable the process button
-    shinyjs::enable("process")
+    observeEvent(input$process, {
+        if (is.null(input$file)) return(NULL)
+        # notify the user that the file is being processed
+        showModal(modalDialog(
+            title = "Please wait",
+            "Your file is being processed ...",
+            easyClose = FALSE
+        ))
 
-    # Remove the modal dialog
-    removeModal()
-  })
+        # Read the file
+        inFile <- input$file
+        result <- generate(inFile$datapath)
+        unlink(inFile$datapath)
+        # Update the result output
+        output$result <- renderPrint({
+            result
+        })
+        # Set is_processing back to FALSE
+        is_processing(FALSE)
+    })
 }
-# 
-# 
-# shinyServer(function(input, output, session) {
-# 
-#   # Input Data
-#   datasetInput <- reactive({
-# 
-#     df <- data.frame(
-#       Name = c("Sepal Length",
-#                "Sepal Width",
-#                "Petal Length",
-#                "Petal Width"),
-#       Value = as.character(c(input$Sepal.Length,
-#                              input$Sepal.Width,
-#                              input$Petal.Length,
-#                              input$Petal.Width)),
-#       stringsAsFactors = FALSE)
-# 
-#     Species <- 0
-#     df <- rbind(df, Species)
-#     input <- transpose(df)
-#     write.table(input,"input.csv", sep=",", quote = FALSE, row.names = FALSE, col.names = FALSE)
-# 
-#     test <- read.csv(paste("input", ".csv", sep=""), header = TRUE)
-# 
-#     Output <- data.frame(Prediction=predict(model,test), round(predict(model,test,type="prob"), 3))
-#     print(Output)
-# 
-#   })
-# 
-#   # Status/Output Text Box
-#   output$contents <- renderPrint({
-#     if (input$submitbutton>0) {
-#       isolate("Calculation complete.")
-#     } else {
-#       return("Server is ready for calculation.")
-#     }
-#   })
-# 
-#   # Prediction results table
-#   output$tabledata <- renderTable({
-#     if (input$submitbutton>0) {
-#       isolate(datasetInput())
-#     }
-#   })
-# 
-# })
+
